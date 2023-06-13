c41a5424e567969f11e24f482029b49d15ca99f9
galaxy4276 <galaxyhi4276@gmail.com>
2023-06-13 22:00:19 +0900

    feat: 모든 엔티티에 Date 핃드를 LocalDate 로 변경

--- src/main/kotlin/com/blueone/app/global/jpa/CreateUpdateDateSet.kt
+++ src/main/kotlin/com/blueone/app/global/jpa/CreateUpdateDateSet.kt
@@ -3,12 +3,12 @@ package com.blueone.app.global.jpa
 import jakarta.persistence.Embeddable
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDate
 
-import java.util.Date
 @Embeddable
 data class CreateUpdateDateSet(
     @CreatedDate
-    var createdDate: Date,
+    var createdDate: LocalDate,
     @LastModifiedDate
-    var updatedDate: Date
+    var updatedDate: LocalDate
 )
